fb97f2872241840d4a154f28f0a80bdb64e2a8bb
22015515 <22015515@TsukasaYuzaki123PC>
2023-07-31 10:15:27 +0800

    HAOYUAN ADDED PAYMENT.JAVA

---
+++ C206_CaseStudy/src/Payment.java
@@ -0,0 +1,22 @@
+
+
+public class Payment extends *INSERT mainn JAVA CLASS THINGY HERE*{
+	private String BidderName;
+
+	public Payment(String PaymentAmount, String PaymentMethod, String BidderName) {
+		super(PaymentAmount, PaymentMethod);
+		this.BidderName = BidderName;
+	}
+
+	public String getBidderName() {
+		return BidderName;
+	}
+	
+	public String toString(){
+
+		// Write your codes here
+		String output = super.toString();
+		output = String.format("%-63s %-20s", output,BidderName);
+		return output;
+	}
+}
