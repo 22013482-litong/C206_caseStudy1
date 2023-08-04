f1d2dd70c75f5917d6820142190e83e88a5ec0ec
turtl <turtl@LAPTOP-935PS8U7.mshome.net>
2023-08-04 13:10:31 +0800

    bbgfgfbfgf

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,38 +1,50 @@
 import java.util.ArrayList;
 
 
+
 public class C206_CaseStudy {
 
-	private static final int OPTION_QUIT = 2;
-	
+	private static final int OPTION_QUIT = 3;
+	private static ArrayList<Auction> auctionList = new ArrayList<Auction>();
+	private static ArrayList<Bidder> bidderList = new ArrayList<Bidder>();
+	private static ArrayList<Administrator> adminList = new ArrayList<Administrator>();
+
 	public C206_CaseStudy(String paymentAmount, String paymentMethod) {
-		// TODO Auto-generated constructor stub
+	
 	}
 
 	public static void main(String[] args) {
+
+	
+
+		auctionList.add(new Auction("Ball Point Pen", "A good pen", 1.50, "4/8/2023", "10/8/2023"));
+
+		bidderList.add(new Bidder("bidder1","password1"));
+		
+		adminList.add(new Administrator("admin1","password0"));
 		
 		int option = 0;
 
 		while (option != OPTION_QUIT) {
-		
-		
-		C206_CaseStudy.menu();
-		option = Helper.readInt("Enter an option > ");
 
-		if (option == 1) {
-		
+			C206_CaseStudy.menu();
+			option = Helper.readInt("Enter an option > ");
 
+			if (option == 1) {
+				// TODO: Implement create account logic
+			} else if (option == 2) {
+				login();
+				
+			}
 		}
 	}
-		
-	}
-			
-	public static void menu() {
-			C206_CaseStudy.setHeader("ONLINE AUCTION APP");
-			System.out.println("1. Log In");
-			System.out.println("2. Quit");
-			Helper.line(80, "-");
 
+	public static void menu() {
+		C206_CaseStudy.setHeader("ONLINE AUCTION APP");
+		System.out.println("1. Create Account");
+		System.out.println("2. Log In");
+		System.out.println("3. Quit");
+		Helper.line(80, "-");
 	}
 
 	public static void setHeader(String header) {
@@ -40,5 +52,26 @@ public class C206_CaseStudy {
 		System.out.println(header);
 		Helper.line(80, "-");
 	}
+	public static void login() {
+        String username = Helper.readString("Enter your username: ");
+        String password = Helper.readString("Enter your password: ");
 
+        
+        for (Bidder bidder : bidderList) {
+            if (bidder.getUsername().equals(username) && bidder.getPassword().equals(password)) {
+                System.out.println("Login successful");
+                return;
+            }
+        }
+
+    
+        for (Administrator admin : adminList) {
+            if (admin.getUsername().equals(username) && admin.getPassword().equals(password)) {
+                System.out.println("Login successful");
+                return;
+            }
+        }
+
+        System.out.println("Invalid username or password");
+    }
 }
